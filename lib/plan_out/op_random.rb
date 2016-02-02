2b8ac2cb652076b5dde8ca7071b2600e972abf76
Ziyad Mir <ziyad@opendoor.com>
2016-02-02 10:19:49 -0800

    breakpoints to test hash generation

--- lib/plan_out/op_random.rb
+++ lib/plan_out/op_random.rb
@@ -16,8 +16,11 @@ module PlanOut
       salty = "#{@mapper.experiment_salt}.#{salt}"
       unit_str = get_unit(appended_unit).join('.')
       x = "#{salty}.#{unit_str}"
+      binding.pry
       last_hex = (Digest::SHA1.hexdigest(x))[0..14]
+      binding.pry
       last_hex.to_i(16)
+      binding.pry
     end
 
     def get_uniform(min_val = 0.0, max_val = 1.0, appended_unit = nil)
